147f839555636be3d3a176b7a93d7e6c75848212
Guslik5 <guslik2003@mail.ru>
2024-10-01 16:31:22 +0300

    added loading animation

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const productRouter = require('./routes/productRoutes')
 const path = require("path"); //root/stan//routes//productRoutes.js
-// const http = require('http')
-// const fs = require('fs')
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,36 +14,11 @@ app.use(express.static(__dirname));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/main.html')
+    res.sendFile(__dirname + '/loadingPage.html')
 })
 
 app.listen(PORT, () => {
     console.log(`Server started: http://localhost:${PORT}`);
 });
-
-// let server = http.createServer((req, res)=>{
-//     res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8', })
-//
-//     const stream = fs.createReadStream('./main.html')
-//     stream.pipe(res)
-// })
-
-
-//
-// app.use(express.static('css'))
-// app.use(express.static('images'))
-// app.use(express.static('js'))
-//
-// app.get('/',(req, res) => {
-//     res.sendFile(__dirname + '/main.html')
-// })
-//
-// app.get('/products',(req, res) => {
-//     res.sendFile(__dirname + '/products.html')
-// })
-//
-// app.get('/user/:username/:id', (req, res) => {
-//     res.send(`User id: ${req.params.id}. UserName: ${req.params.username}` )
-// })
 
 
